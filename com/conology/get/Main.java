1504af9c79bf2c3f7d4a5aa79187deebb3303a00
rokpusnik <49006425+rokpusnik@users.noreply.github.com>
2019-06-20 12:36:22 +0200

    Update Main.java

--- com/conology/get/Main.java
+++ com/conology/get/Main.java
@@ -1,96 +1,25 @@
 package com.conology.get;
 
-import java.util.Base64;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 
-public class MainTest {
-
+public class Main {
     public static void main(String[] args) throws Exception {
-        String original = "rok.pusnik@conology.onmicrosoft.com:nZvnprWZ234FK4RjIDWk32E6";
-        String encoded = Base64.getEncoder().encodeToString(original.getBytes());
-        String authString = "Authorization: Basic ".concat(encoded);
-        String contentType = "Content-Type: application/json";
-        String testAPIGET = "https://conology.atlassian.net/rest/api/2/issue/TRA-144";
-    
-	//java.io way of trying to authenticate
-        URL url = new URL(testAPIGET);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type","application/json");
-        con.setRequestProperty("Authorization", "Basic "+encoded);
-
-        //for POST
-        con.setDoOutput(true);
-        OutputStream os = con.getOutputStream();
-        os.write("grant_type=password&username=someusername&password=somepassword&scope=profile".getBytes()); 
-	//line 30 is very wrong, Jira doesn't need this... but it does need something in its place?
-        os.flush();
-        os.close();
-        // end POST
+    	String email = "rok.pusnik@conology.onmicrosoft.com", token= "nZvnprWZ234FK4RjIDWk32E6";
+    	String jqlQueryURL = "https://conology.atlassian.net/rest/api/2/search?jql=sprint%20in%20openSprints()%20and%20sprint%20not%20in%20futureSprints()";
+    	HttpResponse<JsonNode> response = Unirest.get(jqlQueryURL).basicAuth(email, token).header("Accept","application/json").asJson();
+       	System.out.println(response.getBody());
     	
-        int responseCode = con.getResponseCode();
-        System.out.println("POST Response Code :: " + responseCode);
-        if (responseCode == HttpURLConnection.HTTP_OK) { //success
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-            // print result
-            System.out.println(response.toString());
-        } else {
-            System.out.println("POST request not worked");
-        }  
-	//end of java.io way of trying to authenticate
-	    
-	    
-      ObjectMapper mapper = new ObjectMapper();
-      ReadWebPage page = new ReadWebPage();
-      String content = page.getWebContent(testAPIGET);
-      System.out.println(content);
-	    /*
-       * Post[] posts = mapper.readValue(content, Post[].class);
-	     * System.out.println(posts[4].body);
-       */
+    	//sauce for not working example: https://stackoverflow.com/questions/45687588/java-post-request-to-get-token-from-web-service
+    	    
+    	//just reading from existing JSON file
+    	ObjectMapper mapper = new ObjectMapper();
+    	ReadWebPage page = new ReadWebPage();
+    	String content = page.getWebContent(jqlQueryURL);
+    	
+    	Sprint sprint = mapper.readValue(content, Sprint.class);
+    	
     }
-    /*
-     * send this curl to jira?
-     * curl -D- -u rok.pusnik@conology.onmicrosoft.com:nZvnprWZ234FK4RjIDWk32E6 -X GET -H "Content-Type: application/json" "https://conology.atlassian.net/rest/api/2/issue/TRA-144"
-     */
-    
-    //need to first grab active sprint, then all issues from the sprint, then data from specific issues
-    
-    
-    /* it's HTML time
-     * HTTP:      CRUD: 
-     * POST       Create new entity 
-     * GET        Read list or single entity
-     * PUT        Update existing entity
-     * DELETE     Delete an existing entity
-     * 
-     * HTTP response status codes
-     * 100-199 - informational
-     * 200-299 - successful
-     * 300-399 - redirection
-     * 400-499 - client error
-     * 500-599 - server error
-     * 
-     * MIME content types
-     * Multi-purpose Internet Mail-Extension
-     * type/sub-type 
-     *
-     * text/html
-     * text/plain
-     * 
-     * application/json
-     * application/xml
-     */
-
 }
